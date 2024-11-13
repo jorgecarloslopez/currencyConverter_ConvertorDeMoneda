92c5cfc897834144bc2875f4bed5430f080260a6
jorgecarloslopez <lopez.cs.jorge@gmail.com>
2024-11-12 23:58:32 -0300

    Get the values entered by the user

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const tasasDeCambio = {
   eur: 0.92, // 1local currency = 0.92 EUR
   gbp: 0.8, // 1 local currency= 0.80 GBP
 };
+
+
+
+function convertir() {
+   // Get the values ​​entered by the user
+    const cantidad = parseFloat(document.getElementById("cantidad").value);
+    const moneda = document.getElementById("moneda").value;
