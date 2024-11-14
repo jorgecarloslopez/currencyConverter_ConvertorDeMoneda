0e04411c444d5a0b495057d08da30dc509dc0a01
jorgecarloslopez <lopez.cs.jorge@gmail.com>
2024-11-13 23:35:20 -0300

    correcting line 4 in javascript, syntax error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const tasasDeCambio = {
   usd: 1.1, // 1 local currency= 1.10 USD
   eur: 0.92, // 1local currency = 0.92 EUR
-  gbp: 0.8, // 1 local currency= 0.80 GBP
+  brl: 0.8, // 1 local currency= 0.80 GBP
 };
 
 function convertir() {
